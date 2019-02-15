86c9dd59ed759988e5246a7b391910f86ffb60f0
Rob <robmonga2@hotmail.com>
2019-02-15 13:52:49 -0800

    first commit

---
+++ themes/inhabitent/footer.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * @package RED_Starter_Theme
+ */
+
+?>
+
+			</div><!-- #content -->
+
+			<footer id="colophon" class="site-footer" role="contentinfo">
+				<div class="site-info"> 
+				<p>contact info</p>
+				<br>
+				<p>info@inhabitent.com</p>
+				<br>
+				<p>778-456-7891</p>
+				<br>
+				<p>Business Hours</p>
+				<p>Copyright &#169; 2019 Inhabitent</p>
+					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
+				</div><!-- .site-info --> 
+				
+
+			</footer><!-- #colophon -->
+		</div><!-- #page -->
+
+		<?php wp_footer(); ?>
+
+	</body>
+</html>
---
+++ themes/inhabitent/inc/extras.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Custom functions that act independently of the theme templates.
+ *
+ * @package RED_Starter_Theme
+ */
+
+/**
+ * Adds custom classes to the array of body classes.
+ *
+ * @param array $classes Classes for the body element.
+ * @return array
+ */
+function red_starter_body_classes( $classes ) {
+	// Adds a class of group-blog to blogs with more than 1 published author.
+	if ( is_multi_author() ) {
+		$classes[] = 'group-blog';
+	}
+
+	return $classes;
+}
+add_filter( 'body_class', 'red_starter_body_classes' );
+// Change the wp-admin login page header image
+function RED_Starter_Theme_login_logo() {
+	$logo_url = get_template_directory_uri().'/assets/images/inhabitent-logo-text-dark.svg';
+	
+	echo '<style>
+	.login h1 a {
+		background-image: url(' .$logo_url. ');
+		background-repeat: no-repeat;
+		background-size: 300px 53px;
+		width:300px;
+		height:53px;
+	}
+
+	.login .button.button-primary {
+		background-color: #248A83;
+	}
+
+	</style>';
+}
+	
+	add_action('login_head', 'RED_Starter_Theme_login_logo');
+
+	function red_starter_login_url() {
+		return get_site_url();
+
+	}
+
+	add_filter('login_headerurl', 'red_starter_login_url');
